cde25141a8d7083defac6ad100914490df734cce
justinlm <justin.168@163.com>
2017-02-24 20:20:04 +0800

    BoxCollider测试。

---
+++ Assets/scripts/BoxColliderTest/AIFollowUseBoxCollider.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class AIFollowUseBoxCollider : MonoBehaviour
+{
+
+    public GameObject m_FollowTarget = null;
+
+    public GameObject missile;
+
+    public float missileSpeed = 6f;
+
+    public float missileRotateSpeed = 4f;
+
+    bool whehterShooted = false;
+
+    float distance;
+
+    float collisionDistance;
+
+    // Use this for initialization
+
+    void Start()
+    {
+        missile = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))//按下S 发射导弹
+        {
+            whehterShooted = true;
+        }
+
+        if (whehterShooted && missile != null)
+        {
+            distance = Vector3.Distance(m_FollowTarget.transform.position, missile.transform.position);
+
+            ////导弹朝向人  法一
+            //missile.transform.LookAt(man.transform);          
+
+            //导弹朝向人  法二            
+            Quaternion missileRotation = Quaternion.LookRotation(m_FollowTarget.transform.position - missile.transform.position, Vector3.up);
+            //missile.transform.rotation = Quaternion.Slerp(missile.transform.rotation, missileRotation, Time.deltaTime * missileRotateSpeed);
+            missile.transform.rotation = missileRotation;
+
+
+            //导弹朝向人   法三            
+            //Vector3 targetDirection = man.transform.position - missile.transform.position;
+            //float angle = Vector3.Angle(targetDirection,missile.transform.forward);//取得两个向量间的夹角
+            //print("angle:"+angle.ToString());
+            //if (angle > 5)
+            //{
+            //    missile.transform.Rotate(Vector3.up, angle);
+            //}
+
+            missile.transform.Translate(Vector3.forward * Time.deltaTime * missileSpeed);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("========================:" + collision.collider.name);
+    }
+}
---
+++ Assets/scripts/BoxColliderTest/Target.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    public GameObject m_FollowTarget = null;
+
+    public float manSpeed = 6f;
+
+	// Use this for initialization
+	void Start () {
+        m_FollowTarget = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            m_FollowTarget.transform.Translate(Vector3.left * Time.deltaTime * manSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            m_FollowTarget.transform.Translate(-Vector3.left * Time.deltaTime * manSpeed);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            m_FollowTarget.transform.Translate(Vector3.forward * Time.deltaTime * manSpeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            m_FollowTarget.transform.Translate(-Vector3.forward * Time.deltaTime * manSpeed);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("========================:" + collision.collider.name);
+    }
+}
