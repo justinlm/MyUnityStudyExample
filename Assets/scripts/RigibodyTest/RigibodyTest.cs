36dcd4d959668ae69228e3c736638104d54b8fab
justinlm <justin.168@163.com>
2017-03-02 11:44:05 +0800

    刚体使用测试:moveRotation 和 Quaternion.LookRotation 来做汽车跳跃起来角度旋转变化。

---
+++ Assets/scripts/RigibodyTest/RigibodyTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigibodyTest : MonoBehaviour {
+
+    Rigidbody m_Rigidbody;
+    public float liftAngle = 0;
+    public float addAngle = 1.0f;
+	// Use this for initialization
+	void Start () {
+        m_Rigidbody = this.transform.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+
+        UpdateJumpingRotation(addAngle, dt);
+    }
+
+    void UpdateJumpingRotation(float coeff, float dt)
+    {
+        //Quaternion.LookRotation(rotTan)
+        //创建一个旋转，沿着forward, 并且头部沿着upwards 的约束注视。也就是建立一个旋转，使视角朝向forward，顶部朝向upwards。
+        //使用Rigidbody.MoveRotation来旋转刚体，带有刚体的插值设置。在任意两帧之间平滑过渡
+        Vector3 rotTan = new Vector3(gameObject.transform.forward.x, liftAngle, gameObject.transform.forward.z);
+        m_Rigidbody.MoveRotation(Quaternion.LookRotation(rotTan));
+        liftAngle += dt * coeff;
+        liftAngle = Mathf.Clamp(liftAngle, 0.0f, 0.5f);
+    }
+}
